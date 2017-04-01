a53ae885869819e8f416aced73bc96ab519d38d0
litao <litaonuaa@foxmail.com>
2017-04-02 00:25:18 +0800

    添加了ImageView空间

--- app/src/main/java/com/example/uiview/MainActivity.java
+++ app/src/main/java/com/example/uiview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uiview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button1);
         editText = (EditText)findViewById(R.id.edittext1);
